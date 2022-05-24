37d91815d0dc79ef82d3c4ec7e3ff37b7644a2d1
Tarsis <tarsis.souza@ifal.edu.br>
2022-05-24 11:02:00 -0300

    Aula 08 - Interfaces Gráficas - 24/05
    Nesta aula foram abordados elementos de interface gráfica por meio do  desenvolvimento de telas semelhantes ao aplicativo Hotel

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,9 +16,7 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Color(0xFF10397B),
         title: Text(
           'Pesquisar',
-          style: TextStyle(
-            fontSize: 24
-          ),
+          style: TextStyle(fontSize: 24),
         ),
       ),
       backgroundColor: Colors.grey[100],
@@ -27,110 +25,155 @@ class _HomePageState extends State<HomePage> {
         child: ListView(
           children: [
             Container(
-                padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
                 color: Colors.purple,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text(
-                            'Tops destinos mais buscados',
-                            style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            'Corre que tá rolando muita promoção',
-                            style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(height: 16),
-                          ElevatedButton(
-                            onPressed: () {},
-                            child: Text(
-                              'EU QUERO!',
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.black,
-                              ),
-                            ),
-                            style: ElevatedButton.styleFrom(
-                              primary: Color(0xFFF8FF04),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    SizedBox(width: 24),
-                    Placeholder(
-                      fallbackHeight: 150,
-                      fallbackWidth: 100,
-                      color: Colors.white,
-                    ),
-                  ],
-                )),
-            SizedBox(height: 16),
-            Card(
-              child: Column(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              padding: const EdgeInsets.all(16),
+              child: Row(
                 children: [
-                  Placeholder(fallbackHeight: 150),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
+                  Expanded(
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
                         Text(
-                          'Pacote Cancún 2022',
+                          'Tops destinos mais buscados',
                           style: TextStyle(
-                            fontSize: 21,
+                            fontSize: 24,
+                            color: Colors.white,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        SizedBox(height: 8),
-                        Text('Aéreo - Hotel All inclusive'),
-                        SizedBox(height: 8),
-                        Row(
-                          children: [
-                            Icon(Icons.wb_sunny_outlined),
-                            SizedBox(width: 4),
-                            Text('05 Diárias'),
-                            SizedBox(width: 8),
-                            Icon(Icons.person_outline),
-                            SizedBox(width: 4),
-                            Text('01 Pessoa'),
-                          ],
-                        ),
-                        SizedBox(height: 8),
-                        Text('A partir de R\$ 6816'),
                         Text(
-                          'R\$ 3.749',
+                          'Corre que tá rolando muita promoção',
                           style: TextStyle(
-                            color: Colors.orange,
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.white,
                           ),
                         ),
-                        SizedBox(height: 8),
-                        Text(
-                          'Cancelamento Grátis!',
-                          style: TextStyle(
-                            color: Colors.green[700],
+                        SizedBox(height: 16),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'EU QUERO!',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.black,
+                            ),
                           ),
-                        ),
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xFFF8FF04),
+                          ),
+                        )
                       ],
                     ),
-                  )
+                  ),
+                  SizedBox(width: 24),
+                  Placeholder(
+                    fallbackHeight: 150,
+                    fallbackWidth: 100,
+                    color: Colors.white,
+                  ),
                 ],
               ),
             ),
+            const SizedBox(height: 16),
+            buildCard(
+              imagem:
+              'https://a.cdn-hotels.com/gdcs/production97/d402/6baff29c-e43b-4caf-a9e8-5dcf1279dea0.jpg?impolicy=fcrop&w=800&h=533&q=medium',
+              titulo: 'Pacote Cancún 2022/2023',
+              transporte: 'Aéreo - Hotel All inclusive',
+              numDiarias: 5,
+              numPessoas: 2,
+              numParcelas: 6,
+              precoAntigo: 6819,
+              precoAtual: 2819,
+            ),
+            buildCard(
+              imagem:
+                  'https://letsdive.com.br/wp-content/uploads/2019/04/PROCURANDO-MERGULHO-EM-MARAGOGI-CONSIDERE-MACEIO.png',
+              titulo: 'Pacote Maragogi 2023',
+              transporte: 'Hotel All inclusive',
+              numDiarias: 7,
+              numPessoas: 3,
+              numParcelas: 12,
+              precoAntigo: 4819,
+              precoAtual: 819,
+            ),
+
           ],
         ),
+      ),
+    );
+  }
+
+  buildCard({
+    required String imagem,
+    required String titulo,
+    required String transporte,
+    required int numDiarias,
+    required int numPessoas,
+    required double precoAntigo,
+    required double precoAtual,
+    required int numParcelas,
+  }) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.vertical(
+              top: Radius.circular(8),
+            ),
+            child: Image.network(imagem),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  titulo,
+                  style: TextStyle(
+                    fontSize: 21,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(transporte),
+                SizedBox(height: 8),
+                Row(
+                  children: [
+                    const Icon(Icons.wb_sunny_outlined),
+                    const SizedBox(width: 4),
+                    Text('$numDiarias Diárias'),
+                    const SizedBox(width: 8),
+                    const Icon(Icons.person_outline),
+                    const SizedBox(width: 4),
+                    Text('$numPessoas Pessoas'),
+                  ],
+                ),
+                SizedBox(height: 8),
+                Text('A partir de R\$ $precoAntigo'),
+                Text(
+                  'R\$ $precoAtual',
+                  style: const TextStyle(
+                    color: Colors.orange,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'Cancelamento Grátis!',
+                  style: TextStyle(
+                    color: Colors.green[700],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
